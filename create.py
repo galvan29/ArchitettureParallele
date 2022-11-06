05e3ad5e4c606146eeb5df25fa9c8434af6e2f5e
galvan29 <matteogalvan1999@gmail.com>
2022-11-06 16:35:44 +0100

    First push after call

---
+++ create.py
@@ -0,0 +1,21 @@
+from random import randint
+import sys
+
+file = open("vincoli.txt","a")
+file.truncate(0)
+letterali = int(float(sys.argv[1]))
+vincoli = int(float(sys.argv[2]))
+first_row = "p cnf " + str(letterali) + " " + str(vincoli)
+file.write(first_row)
+
+# generate random integer values
+for i in range(vincoli):
+  value = randint(-letterali, letterali)
+  value2 = randint(-letterali, letterali)
+  while value==0:
+    value = randint(-letterali, letterali)
+  while value2==0 or value2==value or value2==-value:
+    value2 = randint(-letterali, letterali)
+  add =  "\n" + str(value) + " " + str(value2) + " " + str(0)
+  file.write(add)
+file.close()
