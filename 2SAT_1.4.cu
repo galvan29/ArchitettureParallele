c50136a1b25027f1058a1c7de6c2b039ef29cc8a
galvan29 <matteogalvan1999@gmail.com>
2022-11-15 15:56:39 +0100

    s

--- 2SAT_1.4.cu
+++ 2SAT_1.4.cu
@@ -6,8 +6,9 @@
 #include <numeric> 
 #include <cuda.h>
 #include <omp.h>
-#pragma comment(linker, "/STACK:200000000")
-#pragma comment(linker, "/HEAP:200000000")
+#include <malloc.h>
+#include <unistd.h>
+
 using namespace std;
 
 void funcRead(string str[]){
@@ -53,34 +54,30 @@ __device__ void diagonale(bool *d_matrix, int length, int thid){
   d_matrix[(primo*length)+secondo] |= d_matrix[(secondo*length)+primo];
 }
 
-__global__ void prova(bool *d_matrix, int length, bool *d_matrix2, bool *d_matrix3){
+__global__ void prova(bool *d_matrix, int length, long int lengthx2, bool *d_matrix2, bool *d_matrix3){
   int thid2 = blockIdx.x * blockDim.x + threadIdx.x;
-//length numero letterali
   int thid = 0;
-  int doppio = length*length;
 
-  for(int Pass=0; Pass<ceilf((doppio/(blockDim.x*gridDim.x)))+1; Pass++){
+  for(int Pass=0; Pass<ceilf((lengthx2/(blockDim.x*gridDim.x)))+1; Pass++){
     thid = thid2 + Pass*(gridDim.x*blockDim.x);
 
-    if(thid < (doppio)){
+    if(thid < (lengthx2)){
       if(d_matrix[thid]){  //thid = 4
         int secondo = (thid%length);  // 4
         int primo = floorf(thid/length);  // 0
-        for(int i = (secondo*length); i < ((secondo+1)*length); i++){   //da 24
-          //printf("%d %d %d\n", d_matrix[i], (primo+1), i);  
+        for(int i = (secondo*length); i < ((secondo+1)*length); i++){   //da 24 
           if(d_matrix[i] && ((i%length)+1) != (primo+1)){    //24 però 24%6+1 == 1 quindi non entro
             int posizione = (primo*length) + (i%length);
             d_matrix2[posizione] = 1;
           }
         }
         int terzo = 0;
-        if(secondo > (length/2)){
+        if(secondo >= (length/2)){
           terzo = secondo - (length/2);
         }else{
           terzo = secondo + (length/2);
         }
         for(int i = (terzo*length); i < ((terzo+1)*length); i++){   //da 24
-          //printf("%d %d %d\n", d_matrix[i], (primo+1), i);  
           if(d_matrix[i] && ((i%length)+1) != (primo+1)){    //24 però 24%6+1 == 1 quindi non entro
             int posizione = (primo*length) + (i%length);
             d_matrix2[posizione] = 1;
@@ -98,11 +95,12 @@ __global__ void prova(bool *d_matrix, int length, bool *d_matrix2, bool *d_matri
   }
   __syncthreads();
 }
-//LA DIAGONALE NON MI TORNA
+//LA DIAGONALE NON MI TORNA 
 __global__ void checkDiagonale(bool *matrix, int length){
   int thid2 = blockIdx.x * blockDim.x + threadIdx.x;
   int thidCheck1 = 0;
   int thidCheck2 = 0;
+  //printf("%d\n", thid2);
   if(thid2%(length+1) == 0 && thid2 < (length*length)){
     thidCheck1 = thid2;
     thidCheck2 = (length+1)*(length/2) + thid2;
@@ -115,6 +113,23 @@ __global__ void checkDiagonale(bool *matrix, int length){
    printf("Ho controllato la diagonale\n");
   __syncthreads();  
 }
+
+__global__ void findSolution(bool *matrix, int *soluzione, int length, int bit){ //length è nLet tutto per due
+  //il bit sarà quello messo a vero (tipo -1)
+  int thid = blockIdx.x * blockDim.x + threadIdx.x;
+  int lengthDiv2 = length/2;
+  if(thid == 0){
+    if(bit < 0){
+      soluzione[abs(bit)+lengthDiv2-1] = 1;
+      soluzione[abs(bit)-1] = -1;
+    }else{
+      soluzione[bit-1] = 1;
+      soluzione[bit+lengthDiv2-1] = -1;
+    }
+  }
+  __syncthreads();
+}
+
 
 int main(void)
 { 
@@ -131,11 +146,11 @@ int main(void)
   int letterali = stoi(arrayyy[2]);
   int vincoli = stoi(arrayyy[3]);
   int nTotLet = (letterali*2);
-  int nTotLetx2 = nTotLet*nTotLet;
-  bool matrix[nTotLetx2] = {0};
+  long int nTotLetx2 = nTotLet * nTotLet;
+  bool matrix[nTotLetx2];
   string str[vincoli+1];
   funcRead(str);
-  
+
   // #pragma omp parallel shared(str, adj)
   // { 
   //  #pragma omp for schedule(auto)
@@ -157,6 +172,7 @@ int main(void)
       j++;
     }
   }
+
   /*for(int i=0; i<nTotLetx2; i++){
     cout<<matrix[i]<<" ";
     if(i%nTotLet == (nTotLet-1))
@@ -170,31 +186,51 @@ int main(void)
   bool *d_matrix;
   bool *d_matrix2;
   bool *d_matrix3;
+
   cudaMalloc(&d_matrix, nTotLetx2*sizeof(bool));
   cudaMalloc(&d_matrix2, nTotLetx2*sizeof(bool));
   cudaMalloc(&d_matrix3, nTotLetx2*sizeof(bool));
-  cudaDeviceSynchronize();
+
   cudaMemcpy(d_matrix, matrix, nTotLetx2*sizeof(bool), cudaMemcpyHostToDevice);
   cudaMemcpy(d_matrix3, matrix, nTotLetx2*sizeof(bool), cudaMemcpyHostToDevice);
-  cudaDeviceSynchronize();
+  sleep(10);
   //bool out[nTotLet];
   //bool *d_out;
   //cudaMalloc(&d_out, nTotLetx2*sizeof(bool));
-  prova<<<40, 1024>>>(d_matrix, nTotLet, d_matrix2, d_matrix3);
-  cudaDeviceSynchronize();
+  prova<<<40, 1024>>>(d_matrix, nTotLet, nTotLetx2, d_matrix2, d_matrix3);
+  cudaFree(d_matrix);
+  cudaFree(d_matrix2);
   checkDiagonale<<<40, 1024>>>(d_matrix3, nTotLet);
-  cudaDeviceSynchronize();
+
+
+  //PROVIAMO A CERCARE UNA SOLUZIONE
+  int soluzione[nTotLet] = {0};
+  int *d_soluzione;
+  int bit = 1;
+  cudaMalloc(&d_soluzione, nTotLet*sizeof(int));
+  cudaMemcpy(d_soluzione, soluzione, nTotLet*sizeof(int), cudaMemcpyHostToDevice);
+  //findSolution<<<40, 1024>>>(d_matrix3, d_soluzione, nTotLet, bit);
+
+
+
+
   cudaMemcpy(&matrix, d_matrix3, nTotLetx2*sizeof(bool), cudaMemcpyDeviceToHost);
+
+
+
+
   //cudaMemcpy(&out, d_out, nTotLet*sizeof(int), cudaMemcpyDeviceToHost);
   
-  for(int i=0; i<nTotLetx2; i++){
+
+  /*for(int i=0; i<nTotLetx2; i++){
     cout<<matrix[i]<<" ";
     if(i%nTotLet == (nTotLet-1))
       cout<<endl;
   }
   cout<<endl;
-  //}
+  //}*/
 
+  cudaFree(d_matrix3);
   return 0;
 }
 
